5e0ed253f8146409755e3979cf73d291082b0980
hanep77 <yudishan26@gmail.com>
2024-12-23 16:53:53 +0700

    no

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <section id="#">
+    <section id="#" className="min-h-screen flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Belum beres</h1>
     </section>
   )
 }
