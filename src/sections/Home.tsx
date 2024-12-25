a5724bde77e1218145a504c12228f7d51fc61db0
hanep77 <yudishan26@gmail.com>
2024-12-25 10:56:27 +0700

    move note to the top

--- src/sections/Home.tsx
+++ src/sections/Home.tsx
@@ -3,14 +3,14 @@ import Terminal from "../components/Terminal";
 export default function Home() {
   return (
     <section id="#" className="min-h-screen flex flex-col justify-center items-center">
-      <Terminal />
-      <div className="w-80 sm:w-[600px] mt-4 bg-green-300 bg-opacity-10 border border-green-900 rounded p-2">
+      <div className="w-80 sm:w-[600px] mb-2 bg-green-300 bg-opacity-10 border border-green-900 rounded p-2">
         <p>Note:</p>
         <ul className="list-disc ms-6">
-          <li className=""><span className="text-blue-400 font-bold">cat greet.txt</span> in the terminal below</li>
-          <li className=""><span className="text-blue-400 font-bold">clear</span> to clear the terminal</li>
+          <li><span className="text-green-500 font-bold">cat greet.txt</span> in the terminal below</li>
+          <li><span className="text-green-500 font-bold">clear</span> to clear the terminal</li>
         </ul>
       </div>
+      <Terminal />
     </section >
   )
 }
