b93e78e970046ff931b950f7e7cb4389b2f11a0f
hanep77 <yudishan26@gmail.com>
2025-01-09 19:17:19 +0700

    add contact/footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import About from "./sections/About";
+import Contact from "./sections/Contact";
 import Home from "./sections/Home";
 import Projects from "./sections/Projects";
 import Skills from "./sections/Skills";
@@ -15,6 +16,7 @@ export default function App() {
         <About />
         <Skills />
         <Projects />
+        <Contact />
       </main>
     </>
   )
---
+++ src/sections/Contact.tsx
@@ -0,0 +1,25 @@
+import { BsGithub, BsInstagram } from "react-icons/bs";
+import { MdEmail } from "react-icons/md";
+
+export default function Contact() {
+  return (
+    <footer className="bg-zinc-900 text-zinc-400">
+      <div className="mx-auto w-full max-w-screen-md px-4 md:px-0 py-6 lg:py-8">
+        <div className="flex justify-between">
+          <div className="mb-6 md:mb-0">
+            <h1 className="text-2xl font-bold text-slate-100">Hanep</h1>
+          </div>
+          <div className="flex gap-4">
+            <ul>
+              <li><a href="https://instagram.com/hanep77" className="flex items-center gap-1 hover:text-zinc-200"><BsInstagram />Instagram - @hanep77</a></li>
+              <li><a href="https://github.com/hanep77" className="flex items-center gap-1 hover:text-zinc-200"><BsGithub />Github - hanep77</a></li>
+              <li><a href="yudishan@gmail.com" className="flex items-center gap-1 hover:text-zinc-200"><MdEmail />Email - yudishan@gmail.com</a></li>
+            </ul>
+          </div>
+        </div>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="text-sm">©2025 Hanep</span>
+      </div>
+    </footer>
+  )
+}
